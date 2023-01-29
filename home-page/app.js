89799dfb44eca30763480eb7236da26a05486e1b
ibadet ismayilov <ibadetismayilov20@gmail.com>
2023-01-29 03:10:47 +0100

    hall section added to homepage

--- home-page/app.js
+++ home-page/app.js
@@ -6,6 +6,10 @@ const tables = document.querySelector('.tables')
 const change_chat_size = document.querySelector('.change-size');
 const chat_box = document.querySelector('.chatting')
 const tables_container = document.querySelector('.tables-container')
+const halls_button = document.querySelector('.halls-button')
+const hall_section = document.querySelector('.hall-section')
+const leave_halls_button = document.getElementById('exit-hall')
+const animation_background =document.querySelector('.animation-background')
 // const mobile_nav = document.querySelector('.mobile-nav')
 // menu_bar.addEventListener('click', ()=>{
 //     menu_bar.classList.toggle('change')
@@ -14,13 +18,25 @@ const tables_container = document.querySelector('.tables-container')
 
 menu_bar.addEventListener('click', e=>{
     sidebar_desktop.classList.add('open')
+    animation_background.classList.remove('deactive')
+    animation_background.classList.add('coming')
+    animation_background.addEventListener('animationend', e=>{
+        animation_background.classList.remove('coming')
+    }, {once:true})
     // body_section__left.classList.add('narrow')
     // tables.style.gridTemplateColumns = 'repeat(2, 1fr)'
 })
-exit_sidebar.addEventListener('click', e=>{
-    sidebar_desktop.classList.remove('open')
-    // body_section__left.classList.remove('narrow')
-    // tables.style.gridTemplateColumns = 'repeat(3, 1fr)'
+Array.from([exit_sidebar, animation_background]).forEach(el=>{
+    el.addEventListener('click', e=>{
+        sidebar_desktop.classList.remove('open')
+        animation_background.classList.add('leaving')
+        animation_background.addEventListener('animationend',e=>{
+            animation_background.classList.remove('leaving')
+            animation_background.classList.add('deactive')
+        }, {once: true})
+        // body_section__left.classList.remove('narrow')
+        // tables.style.gridTemplateColumns = 'repeat(3, 1fr)'
+    })
 })
 
 change_chat_size.addEventListener('click', e=>{
@@ -34,4 +50,28 @@ change_chat_size.addEventListener('click', e=>{
         change_chat_size.classList.add('maximise')
         change_chat_size.classList.remove('minimise')
     }
+})
+
+halls_button.addEventListener('click', e=>{
+    hall_section.classList.remove('deactive')
+    hall_section.classList.add('coming')
+    animation_background.classList.remove('deactive')
+    animation_background.classList.add('coming')
+    hall_section.addEventListener('animationend', e=>{
+        hall_section.classList.remove('coming')
+        animation_background.classList.remove('coming')
+    }, {once: true})
+})
+
+Array.from([leave_halls_button, animation_background]).forEach(el=>{
+    el.addEventListener('click', e=>{
+        hall_section.classList.add('leaving')
+        animation_background.classList.add('leaving')
+        hall_section.addEventListener('animationend',e=>{
+            hall_section.classList.remove('leaving')
+            hall_section.classList.add('deactive')
+            animation_background.classList.remove('leaving')
+            animation_background.classList.add('deactive')
+        }, {once: true})
+    })
 })
